747c353dd4bcb130c504e8cd242c5c77acc7a3ac
José Fernandes <jose-vitor-7@hotmail.com>
2021-03-09 00:53:58 -0300

    Creating models, controllers and migrations for user and message

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/laravel', function () {
+    return view('laravel');
+});
+
+Route::get('/', function(){
+    return route('api');
 });
